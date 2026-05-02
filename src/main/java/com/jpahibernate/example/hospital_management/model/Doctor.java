bff0f46c7113082cdaa69d7b99c36a46779b884b
moheeb999 <mohibuddin2005@gmail.com>
2026-05-02 12:47:45 +0530

    Added Columns and table and created in db

---
+++ src/main/java/com/jpahibernate/example/hospital_management/model/Doctor.java
@@ -0,0 +1,38 @@
+package com.jpahibernate.example.hospital_management.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity //This represents this class as entity or model class
+@Table(name="doctor")
+@Data // it will add getter and setter
+@NoArgsConstructor//default Constructoprs
+@AllArgsConstructor//Parameterized Constructor
+
+public class Doctor {
+
+    @Id
+    @Column(name="doctor_id",nullable = false)
+    private int id;
+
+    @Column(name ="doctor_name",nullable = false)
+    private String name;
+
+    @Column(name="doctor_email",nullable = false,unique = true)
+    private String email;
+
+
+    @Column(name="doctor_dob",nullable = false)
+    private String dob;
+
+
+    @Column(name="doctor_spec",nullable = false)
+    private String specialization;
+
+}
